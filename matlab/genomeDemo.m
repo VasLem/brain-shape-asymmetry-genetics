9b029dda7e9f5f309b03820d4d572bb02274b408
Vasileios Lemonidis <vasileios.lemonidis@student.kuleuven.be>
2021-10-19 19:04:47 +0200

    Updated Multiple Alleles Graph
    Former-commit-id: c5ae74f9c3f6ed4701744bf5f3de47945d1348ca

--- matlab/genomeDemo.m
+++ matlab/genomeDemo.m
@@ -35,35 +35,54 @@ geno = geno(genoIndex, :);
 [sortedAf, sortedAfArg] = sort(af);
 sortedSnps = snps(sortedAfArg, :);
 %% Decide whether I should remove multi-character allele correspondences (Incorporating Indels)
+close all
 fig = figure("Name", "Occurences of Multi-Character Alleles");
 hold on
-plot(sortedAf);
+yyaxis left
+lHandle = plot(sortedAf);
+set(lHandle, {'DisplayName'}, {'Ordered Frequency'});
+
 oneToMany = strlength(sortedSnps.(4))>1 & strlength(sortedSnps.(5)) == 1;
 manyToMany = strlength(sortedSnps.(4))>1 & strlength(sortedSnps.(5)) > 1;
 manyToOne = strlength(sortedSnps.(4))==1 & strlength(sortedSnps.(5)) > 1;
-scatter(find(manyToOne), 0.9 * ones(1, sum(manyToOne)), 'x');
-scatter(find(manyToMany), 0.8 * ones(1, sum(manyToMany)), 'x');
-scatter(find(oneToMany), 0.7 * ones(1, sum(oneToMany)), 'x');
-legend("Ordered Frequency", ['1->N: ' num2str(sum(oneToMany))], ...
-    ['N->N: ', num2str(sum(manyToMany))]  ,['N->1 :', num2str(sum(manyToOne))]);
-hold off
+edges = linspace(0, size(manyToOne ,1), 50);
+oTMH = histcounts(find(oneToMany),edges);
+mTOH = histcounts(find(manyToOne),edges);
 
+
+sHandle = scatter(find(manyToMany), 0.95 * ones(1, sum(manyToMany))', 'x');
+sHandle.DisplayName = ['N->N: ', num2str(sum(manyToMany))];
+
+yyaxis right
+bHandle = bar( 0.5*(edges(2:end) + edges(1:end-1)), [oTMH', mTOH'], 1);
+bHandle(1).FaceColor = 'blue';
+bHandle(1).DisplayName = ['1->N:  '  num2str(sum(oneToMany))];
+bHandle(1).EdgeColor = 'k';
+bHandle(2).FaceColor = 'red';ax = gca;
+bHandle(2).DisplayName = ['N->1 :', num2str(sum(manyToOne))];
+bHandle(2).EdgeColor = 'k';
+xlabel("Sorted Snps")
+ax = gca;
+ax.YAxis(2).Color = 'k';
+
+legend;
+hold off
 savefig(fig, [RESULTS_DIR 'multi_characters_alleles.fig']);
 saveas(fig, [RESULTS_DIR 'multi_characters_alleles.png']);
 
 
 %% Remove multiallelic for now.
 indelFilter = strlength(snps.(4))==1 & strlength(snps.(5))==1;
-geno = geno(:, indelFilter);
-snps = snps(indelFilter, :);
+genoPruned = geno(:, indelFilter);
+snpsPruned = snps(indelFilter, :);
 
 
 %% Identify the genetic indices for genes with more than one allele
-[rsids , ~, ic] = unique(snps.('RSID'));
+[rsids , ~, ic] = unique(snpsPruned.('RSID'));
 alleleCounts = accumarray(ic,1);
 %% Some checks
 %regarding the fact that positions need to be sorted
-assert(all(sort(snps.POS) == snps.POS));
+assert(all(sort(snpsPruned.POS) == snpsPruned.POS));
 % and that all phenotype ids  correspond to genotype ones
 assert(all((1:length(phenoId)) == phenoIndex'));
 %% Partition SNPs
@@ -77,22 +96,22 @@ assert(all((1:length(phenoId)) == phenoIndex'));
 % phenoPart = pheno.clusterPCAPhenoFeatures{clusterInd};
 phenoWhole = horzcat(pheno.clusterPCAPhenoFeatures{:});
 
-[stats, intStats, intervals] = runCCA(phenoWhole, geno, snps);
+[stats, intStats, intervals] = runCCA(phenoWhole, genoPruned, snpsPruned);
 pThres = 0.05/ size(intStats.ChiSqSignificance, 1);
 
 %%
 significantInts = intervals(intStats.chiSqSignificance<pThres, :);
 starts = significantInts(:,1);
 ends = significantInts(:,2);
-stSnps = snps(starts,:);
-stSnps.Properties.VariableNames = strcat(snps.Properties.VariableNames', '_ST')';
-enSnps = snps(ends,:);
-enSnps.Properties.VariableNames = strcat(snps.Properties.VariableNames', '_EN')';
+stSnps = snpsPruned(starts,:);
+stSnps.Properties.VariableNames = strcat(snpsPruned.Properties.VariableNames', '_ST')';
+enSnps = snpsPruned(ends,:);
+enSnps.Properties.VariableNames = strcat(snpsPruned.Properties.VariableNames', '_EN')';
 
 sigSnpsCoef = zeros(size(significantInts, 1),1);
 sigSnps = table();
 for c=1:size(significantInts,1)
-    intSnps = snps(starts(c):ends(c),:);
+    intSnps = snpsPruned(starts(c):ends(c),:);
     intCoeffs = stats.coeffs(starts(c):ends(c));
     [~, maxCoefArg] = max(abs(intCoeffs));
     sigSnpsCoef(c) = intCoeffs(maxCoefArg);
