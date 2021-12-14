1b0d03146452c7ac7e78f82e6e0a33b1fa0e40aa
Vasileios Lemonidis <vasileios.lemonidis@student.kuleuven.be>
2021-12-14 03:58:15 +0100

    fixes to deployment

--- deploy_matlab.sh
+++ deploy_matlab.sh
@@ -3,9 +3,10 @@
 module purge
 module load matlab/R2019a
 mkdir -p deployed
-cd matlab
+cd deployed
+
 echo 1
-/SOFTWARE/Matlab/R2019a/bin/mcc -mv demoBrainAsymmetryGenome.m -d ../deployed/
+mcc -mv demoBrainAsymmetryGenome.m -d ../deployed/
 echo 2
-/SOFTWARE/Matlab/R2019a/bin/mcc -mv demoBrainAsymmetryHierarchicalClustering.m -d ../deployed/
+mcc -mv demoBrainAsymmetryHierarchicalClustering.m -d ../deployed/
 cd ..
