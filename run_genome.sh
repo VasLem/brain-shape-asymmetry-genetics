ff8f23a2ab36efe0fdc58b0ba4a487dba37167d4
vsc33862 <vsc33862@tier2-p-login-2.genius.hpc.kuleuven.be>
2022-02-10 04:42:56 +0100

    Fixed files, they cannot accept different datasets as inputs

--- run_genome.sh
+++ run_genome.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-fi
 module load worker/1.6.10-intel-2018a
-qsub  -t 1-22  run_genome.pbs $1
+qsub  -t 1-22  run_genome.pbs
--- run_hierarchical.sh
+++ run_hierarchical.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-fi
-qsub run_hierarchical.pbs $1
+qsub -v $1 run_hierarchical.pbs
